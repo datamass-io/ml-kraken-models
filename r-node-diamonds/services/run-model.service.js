bb1fb8874131e48dddba8874e980a1b0e276d09d
Patryk Detko <p.detko@datamass.io>
2019-09-12 14:08:08 +0200

    Fixed run-model

--- r-node-diamonds/services/run-model.service.js
+++ r-node-diamonds/services/run-model.service.js
@@ -10,10 +10,9 @@ module.exports = {
 
 function runModel(inputData) {
 
-    let modelData = JSON.stringify(inputData.modelData);
-    let metaData = JSON.stringify(inputData.metaData);
-
-    //let inputParam = `'${data1}'`;
+    let modelData = inputData.modelData;
+    let metaData = inputData.metaData;
+    
     let actionId = uuid.v1();
 
     return new Promise((resolve, reject) => {
@@ -27,18 +26,20 @@ function runModel(inputData) {
         }, (error, res, body) => {
         if (error) {
             console.error(error)
-            return
+            reject(error);
         }
         console.log(`statusCode: ${res.statusCode}`);
         console.log(body);
+        resolve();
         })
     }).then(() => {
-        const cmd = `Rscript --vanilla --quiet /Users/jwszol/Documents/workspace-git/ml-kraken-models/r-node-diamonds/r-scripts/scripts/runner.R ${inputParam}`;
+        const cmd = `Rscript --vanilla --quiet ./r-scripts/scripts/runner.R ${"'" + JSON.stringify(modelData) + "'"}`;
         const exec = require('child_process').exec;
         return new Promise((resolve, reject) => {
             exec(cmd, (error, stdout, stderr) => {
                 if (error) {
                     console.warn(error);
+                    reject(error);
                 }
                 console.log(stdout);
                 console.log(stderr);
@@ -48,22 +49,23 @@ function runModel(inputData) {
         }
     ).then((response)=> {
         console.log(response);
-        resolve(response);
-    })
-
-    // const cmd = `Rscript --vanilla --quiet /app/r-scripts/scripts/runner.R ${inputParam}`;
-    // const cmd = `Rscript --vanilla --quiet /Users/jwszol/Documents/workspace-git/ml-kraken-models/r-node-diamonds/r-scripts/scripts/runner.R ${inputParam}`;
-    // const exec = require('child_process').exec;
-    // return new Promise((resolve, reject) => {
-    //     exec(cmd, (error, stdout, stderr) => {
-    //         if (error) {
-    //             console.warn(error);
-    //         }
-    //         console.log(stdout);
-    //         console.log(stderr);
-    //         resolve(stdout ? stdout : stderr);
-    //     });
-    // });
-
-    
+        return new Promise((resolve, reject) => {
+            request.post('https://r2bp2xn5b2.execute-api.eu-west-1.amazonaws.com/dev/api/v1/model-action-log', {
+                json: {
+                    modelId: metaData.modelId,
+                    actionId: actionId,
+                    actionType: 'RESPONSE',
+                    body: response
+                }
+            }, (error, res, body) => {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                }
+                console.log(`statusCode: ${res.statusCode}`);
+                console.log(body);
+                resolve(response);
+            });
+        });
+    });
 }
